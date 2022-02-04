9020e737c13c492854a04f25d3e65cfd99fd8c0e
Murtuza <34296103+murtazaalam@users.noreply.github.com>
2022-02-04 16:05:30 +0530

    small changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,11 @@
 import PrimaryNavBar from "./navbar/primaryNavBar/PrimaryNavBar";
 import SecondaryNavBar from "./navbar/secondaryNavBar/NavBar";
-import Footer from "./Footer/Footer";
+import Footer from "./footer/Footer";
+import ContentHeaderImage from "./content-header-image/ContentHeaderImage";
 
-export { PrimaryNavBar, SecondaryNavBar, Footer };
+export { 
+    PrimaryNavBar, 
+    SecondaryNavBar, 
+    Footer,
+    ContentHeaderImage
+ };
