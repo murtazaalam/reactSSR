4a353547565ff190498ffaa9a406c72bfb3a4959
Murtuza <34296103+murtazaalam@users.noreply.github.com>
2022-02-09 10:56:57 +0530

    small changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import PrimaryNavBar from "./navbar/primaryNavBar/PrimaryNavBar";
 import SecondaryNavBar from "./navbar/secondaryNavBar/NavBar";
-import Footer from "./footer/Footer";
 import ContentHeaderImage from "./content-header-image/ContentHeaderImage";
 import ServiceGrid from './serviceGrid/ServiceGrid';
 import Sliderr from "./3DSlider/Sliderr";
@@ -20,7 +19,6 @@ import CourseBody from './courses/courseBody/CourseBody';
 export { 
     PrimaryNavBar, 
     SecondaryNavBar, 
-    Footer,
     ContentHeaderImage,
     Sliderr,
     CourseHeader,
